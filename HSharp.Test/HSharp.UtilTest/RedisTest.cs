6659edf04b0b5f82b29fa9c9ad84461e83897a5c
lidonghui <574427343@qq.com>
2025-01-21 16:21:21 +0800

    console log

--- HSharp.Test/HSharp.UtilTest/RedisTest.cs
+++ HSharp.Test/HSharp.UtilTest/RedisTest.cs
@@ -8,6 +8,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.VisualStudio.TestPlatform.CommunicationUtilities;
+using StackExchange.Redis;
 
 namespace HSharp.UtilTest
 {
@@ -25,7 +26,29 @@ namespace HSharp.UtilTest
 
             };
         }
+        
+       [Test]
+        public void Redis_ZAdd()
+        {
+            var csredis = HSharpRedisContext.GetRedisClient();
+            csredis.ZAdd("lee:demoZSet:Quiz", (79, "Math"));
+            csredis.ZAdd("lee:demoZSet:Quiz", (98, "English"));
+            csredis.ZAdd("lee:demoZSet:Quiz", (87, "Algorithm"));
+            csredis.ZAdd("lee:demoZSet:Quiz", (84, "Database"));
+            csredis.ZAdd("lee:demoZSet:Quiz", (59, "Operation System"));
 
+            //返回集合中的元素数量
+            var aa = csredis.ZCard("lee:demoZSet:Quiz"); 
+
+            // 获取集合中指定范围(90~100)的元素集合
+            var list1=  csredis.ZRangeByScore("lee:demoZSet:Quiz", 90, 100);
+
+            // 获取集合所有元素并升序排序
+            var list2 = csredis.ZRangeWithScores("lee:demoZSet:Quiz", 0, -1);
+
+            // 移除集合中的元素
+            //csredis.ZRem("lee:demoZSet:Quiz", "Math");
+        }
         [Test]
         public void Redis_Set()
         {
@@ -33,6 +56,14 @@ namespace HSharp.UtilTest
             inst.Set("lee:demo:testString", "1232222");
 
             var aa = inst.Get("lee:demo:testString");
+        }
+        [Test]
+        public void Redis_Set_expire()
+        {
+            var inst = HSharpRedisContext.GetRedisClient();
+            inst.Set("lee:demoExpire:testString", "6666666", 60);
+            //Task.Delay(15000).Wait();
+            var aa = inst.Get("lee:demoExpire:testString");
         }
         [Test]
         public void Redis_Get()
